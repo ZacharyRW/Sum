b1c7034cee72004f5aea8831af469b6438c94abb
Zachary Williams <zachary.r.williams@gmail.com>
2020-07-15 16:50:56 -0500

    Changed from sum to sum2 to avoid overriding built in sum

--- Sum.py
+++ Sum.py
@@ -1,9 +1,9 @@
 x = int(input('Enter first number:'))
 y = int(input('Enter second number:'))
-sum = x+y
-print(sum)
+sum2 = x + y
+print(sum2)
 
 w = input('Enter first number:')
 z = input('Enter second number:')
-sum = int(w) + int(z)
-print(sum)
+sum2 = int(w) + int(z)
+print(sum2)
