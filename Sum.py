2594549f3b824a4f471cbfe5ee2fab79c4436841
Zachary Williams <zachary.r.williams@gmail.com>
2020-07-14 20:33:19 -0500

    Main python file

---
+++ Sum.py
@@ -0,0 +1,9 @@
+x = int(input('Enter first number:'))
+y = int(input('Enter second number:'))
+sum = x+y
+print(sum)
+
+w = input('Enter first number:')
+z = input('Enter second number:')
+sum = int(w) + int(z)
+print(sum)
